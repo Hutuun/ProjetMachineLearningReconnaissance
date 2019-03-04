2674333855ed3251ad9300799e2121bd709a993e
jvittone <40358640+jvittone@users.noreply.github.com>
2019-03-04 20:05:16 +0100

    maj

--- fonction.py
+++ fonction.py
@@ -7,6 +7,7 @@ Created on Wed Feb 13 08:40:04 2019
 """
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.neighbors import NearestNeighbors
 
 #Il s'agit de la boucle d'appel sur les classes
 def calculBaryClasse(C):
@@ -45,8 +46,53 @@ def calculSVM(X,Y,A,B):
 	erreur=0
 	for i in range(len(A)): 
 		x=clf.predict([A[i]])
-		if x==B[i]:
+		if x!=B[i]:
 			erreur=erreur+1
 
 	print("Taux d'erreur du SVM : ")
+	print(((erreur*1.0)/(len(A)*1.0))*100)
+	
+def calculPointProche(X,Y,A,B):
+	erreur=0
+	neigh = NearestNeighbors(n_neighbors=1)
+	neigh.fit(X)
+	tempo=neigh.kneighbors(A, return_distance=False)
+	for i in range(len(tempo)):
+		if Y[tempo[i]]!=B[i]:
+			erreur=erreur+1
+		
+	print("Taux d'erreur du plus proche voisin : ")
+	print(((erreur*1.0)/(len(A)*1.0))*100)
+	
+def maxi(tempo):
+	res = -1
+	taille = 0
+	for i in range(len(tempo)):
+		if tempo[i]>taille:
+			res=i
+			taille=tempo[i]
+		#elif tempo[i]==taille:
+			#res=-1
+	return res
+
+def choixMoyen(A,Y,nbclasse):
+	tempo=[0]*nbclasse
+	for i in range(len(A)):
+		tempo[Y[A[i]]]+=1
+	res=maxi(tempo)
+	return res
+
+def calcul10PointsProches(X,Y,A,B):
+	erreur=0
+	neigh = NearestNeighbors(n_neighbors=10)
+	neigh.fit(X)
+	tempo=neigh.kneighbors(A, return_distance=False)
+	res=[0]*len(B)
+	for i in range(len(tempo)):
+		res[i]=choixMoyen(tempo[i],Y,nbclasse)
+	for i in range(len(res)):
+		if Y[res[i]]!=B[i]:
+			erreur=erreur+1
+	
+	print("Taux d'erreur des dix plus proches voisins : ")
 	print(((erreur*1.0)/(len(A)*1.0))*100)
--- projet.py
+++ projet.py
@@ -10,12 +10,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import fonction as fct
 import deepPCA as dpca
+
 from sklearn.neighbors import NearestNeighbors
 
 X = np.load("data/trn_img.npy")
 Y = np.load("data/trn_lbl.npy")
 A = np.load("data/dev_img.npy")
 B = np.load("data/dev_lbl.npy")
+
+nbclasse=10
 
 classe = [0]*10
 for i in range(10):
@@ -30,18 +33,18 @@ nbErreur = fct.calculErreur(B,classeTest)
 print("Taux d'erreur du plus proche : ")
 print((nbErreur*1.0)/(len(B)*1.0)*100)
 
-#dpca.PCAcalcul(A,B,X,Y,0.95)
+dpca.PCAcalcul(A,B,X,Y,0.95)
 
-#dpca.PCAcalcul(A,B,X,Y,0.75)
+dpca.PCAcalcul(A,B,X,Y,0.75)
 
-#dpca.PCAcalcul(A,B,X,Y,0.5)
+dpca.PCAcalcul(A,B,X,Y,0.5)
 
-#dpca.PCAcalcul(A,B,X,Y,0.25)
+dpca.PCAcalcul(A,B,X,Y,0.25)
 
-#dpca.PCAcalcul(A,B,X,Y,0.05)
+dpca.PCAcalcul(A,B,X,Y,0.05)
 
-#calculSVM(X,Y,A,B)
+fct.calculSVM(X,Y,A,B)
 
-neigh = NearestNeighbors(n_neighbors=10)
-neigh.fit(X)
-print(neigh.kneighbors(A, return_distance=False) )
+fct.calculPointProche(X,Y,A,B)	
+
+fct.calcul10PointsProches(X,Y,A,B)
