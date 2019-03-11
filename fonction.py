d7c84d0a8d1b034f723729e66c3f3359ee249bef
jvittone <40358640+jvittone@users.noreply.github.com>
2019-03-11 14:25:20 +0100

    Update fonction.py

--- fonction.py
+++ fonction.py
@@ -34,6 +34,7 @@ def calculPlusProche(C,Bary):
 			res=i
 	return res
 	
+#Calcul le nombre d'erreur entre B et Test
 def calculErreur(B,Test):
 	nbErreur=0
 	for i in range(len(B)):
@@ -41,12 +42,18 @@ def calculErreur(B,Test):
 			nbErreur+=1
 	return nbErreur
 	
+#Utilisation de la méthode SVM
 def calculSVM(X,Y,A,B):
+	#Paramétrage de la SVM
 	clf = SVC(gamma='auto')
+	
+	#Entrainement de la SVM
 	clf.fit(X,Y)
-
+	
+	#Calcul du nombre d'erreur
 	erreur=0
 	for i in range(len(A)): 
+		#Calcul de la classe de A
 		x=clf.predict([A[i]])
 		if x!=B[i]:
 			erreur=erreur+1
@@ -55,11 +62,20 @@ def calculSVM(X,Y,A,B):
 	print("Taux d'erreur du SVM : ")
 	print(((erreur*1.0)/(len(A)*1.0))*100)
 	
+#Calcul du point le plus proche à chaque fois et affichage du taux d'erreur 
 def calculPointProche(X,Y,A,B):
 	erreur=0
+	
+	#Indication du nombre de voisins
 	neigh = NearestNeighbors(n_neighbors=1)
+	
+	#Entrainement
 	neigh.fit(X)
+	
+	#Calcul de la classe pour tous les éléments de l'ensemble de développement
 	tempo=neigh.kneighbors(A, return_distance=False)
+	
+	#Calcul du nombre d'erreur
 	for i in range(len(tempo)):
 		if Y[tempo[i]]!=B[i]:
 			erreur=erreur+1
@@ -67,8 +83,9 @@ def calculPointProche(X,Y,A,B):
 	#Affichage du taux d'erreur
 	print("Taux d'erreur du plus proche voisin : ")
 	print(((erreur*1.0)/(len(A)*1.0))*100)
-	
-def maxi(init,tempo):
+
+#Calcul de la position du maximum dans le tableau tempo
+def posMaxi(init,tempo):
 	res = init
 	taille = 0
 	for i in range(len(tempo)):
@@ -79,20 +96,30 @@ def maxi(init,tempo):
 			#res=-1
 	return res
 
+#Calcul des point les plus proches à chaque fois et affichage du taux d'erreur 
 def calculPointsProches(X,Y,A,B,nbclasse,voisins):
 	classe=[0]*10
 	erreur=0
+	
+	#Indication du nombre de voisins
 	neigh = NearestNeighbors(n_neighbors=voisins)
+	
+	#Entrainement
 	neigh.fit(X)
+	
+	#Calcul de la classe pour tous les éléments de l'ensemble de développement
 	tempo=neigh.kneighbors(A, return_distance=False)
+	
+	#Calcul de la classe des différents points de l'ensemble de développement
 	res = [0]*len(tempo)
 	for i in range(len(tempo)):
 		classe=[0]*10
 		for j in tempo[i]:
 			k=Y[j]
 			classe[k]=classe[k]+1
-		res[i]=maxi(Y[tempo[i]],classe)
-			
+		res[i]=posMaxi(Y[tempo[i]],classe)
+	
+	#Calcul du nombre d'erreur
 	for i in range(len(res)):
 		if res[i]!=B[i]:
 			erreur=erreur+1
